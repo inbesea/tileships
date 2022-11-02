31497d16f3310714343db2f66c4f2cf3f83b2c16
Noah Crosby <noah.b.crosby@gmail.com>
2022-11-02 16:38:50 -0500

    Creates GameScreen with basic data

---
+++ core/src/com/ncrosby/game/screens/GameScreen.java
@@ -0,0 +1,45 @@
+package com.ncrosby.game.screens;
+
+import com.badlogic.gdx.Screen;
+import com.ncrosby.game.tileShipGame;
+
+public class GameScreen implements Screen {
+    public GameScreen(final tileShipGame game){
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta){
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
