9e5bbc871d0918c9905eb031a4ae6359595360e4
Noah Crosby <noah.b.crosby@gmail.com>
2022-11-10 10:05:49 -0600

    Deletes Main.java
    Not used in the libGDX framework

--- core/src/com/ncrosby/game/Main.java
+++
@@ -1,9 +0,0 @@
-package com.ncrosby.game;
-
-import com.ncrosby.game.main.legacyGame;
-
-public class Main {
-    public static void main(String[] args) {
-        legacyGame game = new legacyGame();
-    }
-}
