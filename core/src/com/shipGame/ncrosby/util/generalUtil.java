a8a89f4932b1c7bc1b9bdbe2a078f5e8770e28e6
Noah Crosby <noah.b.crosby@gmail.com>
2022-11-20 21:16:17 -0600

    Adds utility method to find the closest Vector2 to a Vector2

--- core/src/com/shipGame/ncrosby/util/generalUtil.java
+++ core/src/com/shipGame/ncrosby/util/generalUtil.java
@@ -2,7 +2,9 @@ package com.shipGame.ncrosby.util;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.shipGame.ncrosby.generalObjects.GameObject;
 
 public class generalUtil {
@@ -16,11 +18,8 @@ public class generalUtil {
      */
     public static int clamp(int var, int min, int max) {
         if (var >= max)
-            return var = max;
-        else if (var <= min)
-            return var = min;
-        else
-            return var;
+            return max;
+        else return Math.max(var, min);
     }
 
     /**
@@ -50,5 +49,38 @@ public class generalUtil {
         position.set(Gdx.input.getX(), Gdx.input.getY(), 0);
         camera.unproject(position);
         return position;
+    }
+
+    /**
+     * Method to find the closest tile to a point given an Array of ShipTiles
+     *
+     * @param location - Vector to check prox. to
+     * @param vectors - Array of Vectors to search in
+     * @return - closest tile
+     */
+    public static Vector2 closestVector2(Vector2 location , Array<Vector2> vectors) {
+        if(vectors.size == 0)return null;
+        if(vectors.size == 1)return vectors.get(0);
+
+        double minDistance = Double.POSITIVE_INFINITY; // First check will always be true
+        Vector2 tempV;
+        Vector2 closestVector = null;
+        Vector3 location3 = new Vector3(location.x, location.y, 0);
+        Vector3 position;
+
+        //Loop through ship to find closest tile
+        for (int i = 0 ; i < vectors.size ; i++){
+            tempV = vectors.get(i);
+            position = new Vector3(tempV.x, tempV.y , 0);
+            float distance;
+            distance = location3.dst(position);
+
+            // Check if distance between position and current tile is shorter
+            if(distance < minDistance){
+                minDistance = distance;
+                closestVector = tempV;
+            }
+        }
+        return closestVector;
     }
 }
