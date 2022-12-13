c56bdd9fb9bef38f4de2fd99049cba3c4d125ed5
Noah Crosby <noah.b.crosby@gmail.com>
2022-12-13 16:30:41 -0600

    Introduces TileCondenser to manage getting new tiles from old ones.
    Core to re-forming your ship and making defending easier.

---
+++ core/src/com/shipGame/ncrosby/generalObjects/Ship/TileCondenser.java
@@ -0,0 +1,33 @@
+package com.shipGame.ncrosby.generalObjects.Ship;
+
+import com.badlogic.gdx.utils.Array;
+import com.shipGame.ncrosby.ID;
+import com.shipGame.ncrosby.generalObjects.Ship.tiles.ShipTile;
+
+/**
+ * Class to build out new instances of tiles based on ordered arrays of tiles.
+ */
+public class TileCondenser {
+
+    Array<ID> TileIDs;
+
+    public TileCondenser(){
+        TileIDs = new Array<>();
+    }
+
+    /**
+     * Returns a tile based on an array of tiles.
+     * @param tiles - array of tiles to condense
+     * @return - a single product tile.
+     */
+    public ShipTile buildNewTile(Array<ShipTile> tiles){
+        if(tiles.isEmpty()){
+            System.out.println("Empty array passed to TileCondenser");
+            return null;
+        } else {
+
+        }
+        return null;
+    }
+
+}
