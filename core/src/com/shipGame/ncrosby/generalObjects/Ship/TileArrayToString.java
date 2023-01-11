223b5e263982f771cd8db3019486607b36a0625e
Noah Crosby <noah.b.crosby@gmail.com>
2023-01-11 16:45:07 -0600

    Adds class to generate string representations of tileArrays

---
+++ core/src/com/shipGame/ncrosby/generalObjects/Ship/TileArrayToString.java
@@ -0,0 +1,50 @@
+package com.shipGame.ncrosby.generalObjects.Ship;
+
+import com.badlogic.gdx.utils.Array;
+import com.shipGame.ncrosby.generalObjects.Ship.tiles.ShipTile;
+import com.shipGame.ncrosby.generalObjects.Ship.tiles.TileOrienter;
+
+/**
+ * Takes an array of ShipTiles and can return a string representation of said array.
+ *
+ * String example can be : "std0std1std1std2"
+ * This represents four standard tiles at (0,0),(0,1),(1,1),(1,0) going up right down
+ */
+public class TileArrayToString {
+
+    private Array<ShipTile> tiles;
+
+    public TileArrayToString(Array<ShipTile> tiles){
+        this.tiles = tiles;
+    }
+
+
+
+    /**
+     * Generates a string for ShipTile development comparison.
+     * @return
+     */
+    public String toCompareString() {
+        // Create a string representing the array passed to this object, where orientation is up.
+        TileOrienter orienter = new TileOrienter(tiles);
+        int orientation = orienter.setAndReturnCurrentOrientation(0);
+        // We have the orientation now. I want to scribe each tile individually when looping.
+        // The orienter may just move the array over and ruin the actual tile in the ship. Can we do that differently.
+        // tilea tileB tileC have indexes and the first two point right. Lets make a string out of them.
+        if(orientation != TileOrienter.INVALID){
+            tileToString(orienter.getTileArray());
+        }
+        return null;
+    }
+
+    /**
+     * Takes arbitrary array of tiles and returns string representation.
+     * @param tiles
+     * @return
+     */
+    private String tileToString(Array<ShipTile> tiles) {
+
+
+        return null;
+    }
+}
