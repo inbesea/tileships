ef9ac020dd046ac4b7e36607976d61f8c9030a67
Noah Crosby <noah.b.crosby@gmail.com>
2023-01-18 16:36:12 -0600

    Adds static orientation calculation method to TileOrienter
    Cleans up dead code too

--- core/src/com/shipGame/ncrosby/generalObjects/Ship/tiles/TileOrienter.java
+++ core/src/com/shipGame/ncrosby/generalObjects/Ship/tiles/TileOrienter.java
@@ -28,6 +28,17 @@ public class TileOrienter {
     }
 
     /**
+     * Constructor to allow orientation without an internal array
+     * @param currentOrientation - assumed orientation to move from.
+     */
+    public TileOrienter(int currentOrientation){
+        this.tileArray = null;
+        this.tileOne = null;
+        this.tileTwo = null;
+        this.currentOrientation = currentOrientation;
+    }
+
+    /**
      * reviews the first pair of tiles to parse their relative orientation and return an int representing orientation.
      *
      * This is based on the index's relationship to one another, and they must be orthogonally adjacent
@@ -58,73 +69,47 @@ public class TileOrienter {
         } else if(tile0Index.x - 1 == tile1Index.x && ySame) { // Left
             return setAndReturnCurrentOrientation(AdjacentTiles.LEFT);
         } else { // Is reached if the array is too short
-            Gdx.app.debug("CondenserMessage", "Incomparable tiles passed to GetOrientation()" + tile0Index + " " + tile1Index);
+            Gdx.app.debug("CondenserMessage", "Incomparable tiles passed to calculateOrientation()" + tile0Index + " " + tile1Index);
             result = -1;
             return result;
         }
     }
 
     /**
-     * Method to rotate the currently set array around it's first tile to point in a direction.
-     * The first two tiles are where the direction is initially.
-     * Rotating them in relationship to one another determines the overall new tile positions.
+     * reviews the first pair of tiles to parse their relative orientation and return an int representing orientation.
+     *
+     * This is based on the index's relationship to one another, and they must be orthogonally adjacent
+     * The result is only meant to be a way to match the shape to a recipe so orientation of the containing ship should not matter.
+     *
+     * @return Returns 0-3 representing orientation between the two tiles. -1 if invalid inputs.
      *      * 0 == Up
      *      * 1 == Right
      *      * 2 == Down
      *      * 3 == Left
-     *
-     * @param direction - int describing the new direction to point the tile array
-     * @return - newly oriented array, null if no orientation can be done.
+     *      * -1 == not enough tiles, or invalid
      */
-    public Array<ShipTile> orientArray(int direction){
-        if(4 > currentOrientation && currentOrientation > -1){ // Check that current orientation is valid
+    public static int calculateOrientation(ShipTile tileOne, ShipTile tileTwo) {
+        int result;
+        Vector2 tile0Index = new Vector2(tileOne.getxIndex(), tileOne.getyIndex());
+        Vector2 tile1Index = new Vector2(tileTwo.getxIndex(), tileTwo.getyIndex());
 
-            if(currentOrientation == direction)return tileArray; // Check for no rotation needed
+        boolean xSame = tile0Index.x == tile1Index.x;
+        boolean ySame = tile0Index.y == tile1Index.y;
 
-            /* If current orientation and direction are valid and do not match then we can begin orientation */
-            else if(direction == AdjacentTiles.UP){ // Check needed direction
-                return orientUpwards();
-            } else if(direction == AdjacentTiles.RIGHT) {
-                return orientRight();
-            } else if (direction == AdjacentTiles.DOWN){
-                return orientDown();
-            } else if (direction == AdjacentTiles.LEFT){
-                return orientLeft();
-            } else {
-                return null;
-            }
-
+        // Addition
+        if(tile0Index.y + 1 == tile1Index.y && xSame){ // Up
+            return AdjacentTiles.UP;
+        } else if(tile0Index.x + 1 == tile1Index.x && ySame) { // Right
+            return AdjacentTiles.RIGHT;
+        } else if(tile0Index.y - 1 == tile1Index.y && xSame) { // Down
+            return AdjacentTiles.DOWN;
+        } else if(tile0Index.x - 1 == tile1Index.x && ySame) { // Left
+            return AdjacentTiles.LEFT;
+        } else { // Is reached if the array is too short
+            Gdx.app.debug("TileOrienter", "Incomparable tiles passed to calculateOrientation()" + tile0Index + " " + tile1Index);
+            result = -1;
+            return result;
         }
-        return null;
-    }
-
-    private Array<ShipTile> orientLeft() {
-        return null;
-    }
-
-    private Array<ShipTile> orientDown() {
-        return null;
-    }
-
-    private Array<ShipTile> orientRight() {
-        return null;
-    }
-
-    private Array<ShipTile> orientUpwards() {
-        return null;
-    }
-
-
-    private void flipXYIndexes(){
-
-    }
-
-    /**
-     * Will multiply the x or y indexes to the opposite
-     * @param xOrY
-     */
-    private void switchXYPolarity(int xOrY){
-
     }
 
     public int getCurrentOrientation() {
@@ -140,25 +125,6 @@ public class TileOrienter {
         return currentOrientation;
     }
 
-//    /**
-//     * Method to create a new array of tiles with different orientation and return it while setting the tileOrienter's
-//     * local array to the new value.
-//     * @param newOrientation
-//     * @return
-//     */
-//    public Array<ShipTile> rotateAndReturnArrayOrientation(int newOrientation) {
-//        this.currentOrientation = newOrientation;
-//        Array<ShipTile> newArray = new Array<>();
-//        ShipTile temp;
-//
-//        for(int i = 0 ; i < tileArray.size ; i++){
-//            temp = tileArray.get(i);
-//            newArray.add(new ShipTile()); // I need to turn each tile's location.
-//            // We are using neighbors to figure out what the relationships are, but that requires the Ship object to do.
-//            // Actually this is a better reason to just rotate each individually. for the string...
-//        }
-//        return null;
-//    }
 
     /**
      * Getter for the local tile Array
