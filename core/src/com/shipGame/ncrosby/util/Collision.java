488e0c70f20a994465e5dd502c3450a22ce1ee5e
Noah Crosby <noah.b.crosby@gmail.com>
2023-06-04 21:26:37 -0500

    Adds Collision.java
    This is for passing collision events to the collision handler

---
+++ core/src/com/shipGame/ncrosby/util/Collision.java
@@ -0,0 +1,18 @@
+package com.shipGame.ncrosby.util;
+
+import com.shipGame.ncrosby.ID;
+import com.shipGame.ncrosby.generalObjects.GameObject;
+
+public class Collision {
+    GameObject colliderA;
+    GameObject colliderB;
+
+    public Collision(GameObject a, GameObject b){
+        this.colliderA = a;
+        this.colliderB = b;
+    }
+
+    public boolean hasType(ID id){
+        return colliderA.getID() == id || colliderB.getID() == id;
+    }
+}
---
+++ core/test/com/shipGame/ncrosby/util/CollisionTest.java
@@ -0,0 +1,35 @@
+package com.shipGame.ncrosby.util;
+
+import com.badlogic.gdx.math.Vector2;
+import com.shipGame.ncrosby.ID;
+import com.shipGame.ncrosby.generalObjects.GameObject;
+import com.shipGame.ncrosby.generalObjects.Ship.tiles.StandardTile;
+import com.shipGame.ncrosby.generalObjects.Ship.tiles.StrongTile;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CollisionTest extends TestCase {
+
+    GameObject gameObjectA;
+    GameObject gameObjectB;
+    Collision collision;
+
+    public void setUp(){
+        // Going to need both types for testing different scenarios
+        gameObjectA = new StandardTile(new Vector2(0,0));
+        gameObjectB = new StrongTile(new Vector2(0,0));
+        collision = new Collision(gameObjectA, gameObjectB);
+    }
+
+    @Test
+    public void testIfGameObjectMatchesOneOrBoth_ReturnsTrue(){
+        // Before - have two tiles colliding
+        // When - checking types
+        // Then - we get appropriate assertions back
+        assertTrue(collision.hasType(ID.StrongTile));
+        assertTrue(collision.hasType(ID.StandardTile));
+        assertFalse(collision.hasType(ID.CoreTile));
+    }
+}
