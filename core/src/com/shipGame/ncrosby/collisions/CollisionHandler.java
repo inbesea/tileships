ab582f34d66a7c9f6d0dfc7c5e906ea0efd8e424
noahCrosby <noah.b.crosby@gmail.com>
2023-06-12 11:40:58 -0500

    Adds basic collision handling operations in CollisionHandler

--- core/src/com/shipGame/ncrosby/collisions/CollisionHandler.java
+++ core/src/com/shipGame/ncrosby/collisions/CollisionHandler.java
@@ -1,7 +1,59 @@
 package com.shipGame.ncrosby.collisions;
 
 public class CollisionHandler {
+    GameObject a;
+    GameObject b;
     public void handleCollision(Collision collision){
+        a = (GameObject) collision.colliderA;
+        b = (GameObject) collision.colliderB;
+        if(numberOfCollisionsTiles(a,b) > 0){ //
+            if(numberOfAsteroidsInCollision(a,b) > 0){
+                asteroidTileBounce(a, b);
+            }
+        } else { // there are no tiles
 
+        }
+
+        // Finish handling Collision. Would like to know if there's an assignment problem  here.
+        a = null;
+        b = null;
+    }
+
+    private int numberOfAsteroidsInCollision(GameObject a, GameObject b) {
+        int i = 0;
+        if(a.getID() == ID.Asteroid) i++;
+        if(b.getID() == ID.Asteroid) i++;
+        return i;
+    }
+
+    private int numberOfCollisionsTiles(GameObject a, GameObject b) {
+        int i = 0;
+        if(a.getID().isTileType()) i++;
+        if(b.getID().isTileType()) i++;
+        return i;
+    }
+
+    private void asteroidTileBounce(GameObject a, GameObject b) {
+        Asteroid asteroid;
+        ShipTile tile;
+        // Assign based on what the classes are
+        if(a.getID() == ID.Asteroid){
+            asteroid = (Asteroid) a;
+            tile = (ShipTile) b;
+        } else {
+            asteroid = (Asteroid) b;
+            tile = (ShipTile) a;
+        }
+
+        // Check for core first
+        if(tile.getID() == ID.CoreTile){
+            // Turn the asteroid into a standardTile.
+        } else if (tile.isInvulnerable()){
+            return; // Do nothing, the tile cannot be destroyed
+        } else {
+            // destroy the tile
+            // This means the tile is not a core tile or a invulnerable tile.
+            // We will then conclude it can be destroyed.
+        }
     }
 }
