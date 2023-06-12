180f579638259ab01c30b08845b68fb1c58331d0
noahCrosby <noah.b.crosby@gmail.com>
2023-06-12 15:12:40 -0500

    Creates GameObjectManager class

---
+++ core/src/com/shipGame/ncrosby/managers/GameObjectManager.java
@@ -0,0 +1,8 @@
+package com.shipGame.ncrosby.managers;
+
+/**
+ * This class is meant to be an IO for all in-game objects. We should have references to all objects here.
+ */
+public class GameObjectManager {
+
+}
