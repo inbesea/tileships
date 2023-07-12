6f95f400f25bcfe66b831b382f26ff4daa50f7f0
Raxorg <lrdolucho@gmail.com>
2023-07-12 14:10:39 -0400

    MathUtils comes with overloaded clamp methods :3

--- core/src/com/shipGame/util/generalUtil.java
+++ core/src/com/shipGame/util/generalUtil.java
@@ -3,6 +3,7 @@ package com.shipGame.util;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
@@ -11,32 +12,6 @@ import com.badlogic.gdx.utils.Array;
 import com.shipGame.generalObjects.GameObject;
 
 public class generalUtil {
-
-    /**
-     *  Checks for out of bounds amounts and returns to inbounds
-     *
-     *  @param var - Variable to check
-     *  @param min - value to return when var is smaller than min
-     *  @param max - value to return when var is larger than max
-     */
-    public static int clamp(int var, int min, int max) {
-        if (var >= max)
-            return max;
-        else return Math.max(var, min);
-    }
-
-    /**
-     *  Checks for out of bounds amounts and returns to inbounds
-     *
-     *  @param var - Variable to check
-     *  @param min - value to return when var is smaller than min
-     *  @param max - value to return when var is larger than max
-     */
-    public static float clamp(float var, float min, float max) {
-        if (var >= max)
-            return max;
-        else return Math.max(var, min);
-    }
 
     /**
      *
