e1f6ac6125d3a7ba1eab7e7381a8869ce54de886
Noah Crosby <noah.b.crosby@gmail.com>
2023-10-17 16:49:51 -0500

    Adds classes for Level data and handling.
    Might never be used honestly.

---
+++ core/src/main/java/org/bitbucket/noahcrosby/shipGame/screens/LevelScreen.java
@@ -0,0 +1,27 @@
+package org.bitbucket.noahcrosby.shipGame.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import org.bitbucket.noahcrosby.shipGame.TileShipGame;
+
+public class LevelScreen extends ScreenAdapter implements Screen {
+
+    public LevelScreen(final TileShipGame game) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
