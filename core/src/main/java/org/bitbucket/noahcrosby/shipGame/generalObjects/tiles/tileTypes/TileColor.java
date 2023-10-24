d07a6adf3242860e5ed1068de695fa6e4ee2337b
Noah Crosby <noah.b.crosby@gmail.com>
2023-10-24 10:50:05 -0500

    Creates switch to determine what texture relates to a TileColor

--- core/src/main/java/org/bitbucket/noahcrosby/shipGame/generalObjects/tiles/tileTypes/TileColor.java
+++ core/src/main/java/org/bitbucket/noahcrosby/shipGame/generalObjects/tiles/tileTypes/TileColor.java
@@ -1,5 +1,8 @@
 package org.bitbucket.noahcrosby.shipGame.generalObjects.tiles.tileTypes;
 
+import com.badlogic.gdx.graphics.Texture;
+import org.bitbucket.noahcrosby.javapoet.Resources;
+
 /**
  * This reflects an optional color for a tile to have.
  * Currently used only in Arcade mode
@@ -15,5 +18,26 @@ public enum TileColor {
     private String color;
     TileColor(String color){
         this.color = color;
+    }
+
+    public static Texture getTexture(TileColor tileColor) {
+        switch (tileColor) {
+            case RED:
+                return Resources.ShipTileRedTexture;
+            case ORANGE:
+                return Resources.ShipTileOrangeTexture;
+            case YELLOW:
+                return Resources.ShipTileYellowTexture;
+            case GREEN:
+                return Resources.ShipTileGreenTexture;
+            case BLUE:
+                return Resources.ShipTileBlueTexture;
+            case PURPLE:
+                return Resources.ShipTilePurpleTexture;
+            case SILVER:
+                return Resources.ShipTileSilverTexture;
+            default:
+                return null;
+        }
     }
 }
