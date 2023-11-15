2aeef34a62d0ac775a56f452b4bb5c5f38dee44a
Noah Crosby <noah.b.crosby@gmail.com>
2023-11-15 15:08:56 -0600

    Adds SpaceMap.connectNodes, and SpaceMap.disconnectNodes

--- core/src/main/java/org/bitbucket/noahcrosby/LevelData/SpaceMap.java
+++ core/src/main/java/org/bitbucket/noahcrosby/LevelData/SpaceMap.java
@@ -37,12 +37,40 @@ public class SpaceMap {
                     System.out.println("Trying to connect MapNodes with same indexes : " + otherNodes[i] + " Skipping...");
                     continue;
                 }
-                node.edges.add(temp);
-                temp.edges.add(node);
+                connectNodes(node, temp);
             }catch (IndexOutOfBoundsException e){
                 System.out.println("Trying to connect MapNodes with out of bounds indexes : " + otherNodes[i]);
             }
         }
+    }
+
+    /**
+     * Connects two nodes without duplication
+     *
+     * Will fix one way connections, by checking for missing connections before adding to edges.
+     * @param n1
+     * @param n2
+     * @return
+     */
+    public void connectNodes(MapNode n1, MapNode n2){
+        if(!n1.edges.contains(n2, true)){
+            n1.edges.add(n2);
+        }
+        if(!n2.edges.contains(n1, true)){
+            n2.edges.add(n1);
+        }
+    }
+
+    /**
+     * Disconnects two nodes from each other
+     * @param n1
+     * @param n2
+     * @return
+     */
+    public void disconnectNodes(MapNode n1, MapNode n2){
+        // Will remove connections from both, one, or neither
+        if(n1.edges.contains(n2, true))n1.edges.removeValue(n2, true);
+        if(n2.edges.contains(n1, true))n2.edges.removeValue(n1, true);
     }
 
     public Array<MapNode> getMapNodes() {
