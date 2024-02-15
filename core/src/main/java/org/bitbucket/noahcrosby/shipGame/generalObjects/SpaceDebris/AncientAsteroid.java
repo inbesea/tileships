cf7f25a556f1f0759206ac5c23167d1d218480a9
noahCrosby <noah.b.crosby@gmail.com>
2024-02-15 16:53:24 -0600

    Adds Texture get for AncientAsteroid

--- core/src/main/java/org/bitbucket/noahcrosby/shipGame/generalObjects/SpaceDebris/AncientAsteroid.java
+++ core/src/main/java/org/bitbucket/noahcrosby/shipGame/generalObjects/SpaceDebris/AncientAsteroid.java
@@ -1,8 +1,15 @@
 package org.bitbucket.noahcrosby.shipGame.generalObjects.SpaceDebris;
 
+import com.badlogic.gdx.graphics.Texture;
+import org.bitbucket.noahcrosby.javapoet.Resources;
 import org.bitbucket.noahcrosby.shipGame.generalObjects.tiles.tileTypes.AncientTile;
 
 public class AncientAsteroid extends Asteroid{
+
+    @Override
+    public Texture getTexture() {
+        return Resources.AncientAsteroidTexture;
+    }
 
     @Override
     public Class productionOutput() {
