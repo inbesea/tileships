4da47dc4dccc1ec8352ab3cd102825ee0ce6fcdb
Noah Crosby <noah.b.crosby@gmail.com>
2024-07-15 21:21:28 -0500

    Webhook test

--- core/src/main/java/org/bitbucket/noahcrosby/shipGame/levelData/MapNode.java
+++ core/src/main/java/org/bitbucket/noahcrosby/shipGame/levelData/MapNode.java
@@ -292,7 +292,7 @@ public class MapNode extends GameObject {
     /**
      * Returns number of refuels remaining
      *
-     * @return Integer of refuels remaining
+     * @return Integer of refuels remaining I'm silly
      */
     public Integer getRefuels() {
         return refuels;
