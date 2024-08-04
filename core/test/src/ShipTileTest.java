d7bd96faa8a7cfd9c96ee979097179e6eeb1fca7
Noah Crosby <noah.b.crosby@gmail.com>
2024-08-04 12:19:40 -0500

    Adds test and qodana files

---
+++ core/test/src/ShipTileTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class ShipTileTest {
+    @Test
+    public void test_test(){
+        System.out.println("Testing!");
+        assertTrue(false);
+    }
+}
