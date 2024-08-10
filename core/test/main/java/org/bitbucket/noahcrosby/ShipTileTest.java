8507090efde518cfae78d2ba68dd2c073ea0a02a
Noah Crosby <noah.b.crosby@gmail.com>
2024-08-09 20:27:27 -0500

    Attempts to fix tests and gradle issues.
    It did not do that

---
+++ core/test/main/java/org/bitbucket/noahcrosby/ShipTileTest.java
@@ -0,0 +1,13 @@
+package main.java.org.bitbucket.noahcrosby;
+
+import org.junit.Test;
+import org.junit.*;
+
+public class ShipTileTest {
+   @Test
+    public void test_test(){
+        System.out.println("Testing!");
+        Assert.fail();
+        assert 1 == 2;
+    }
+}
